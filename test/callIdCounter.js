48e5603e503922822760b9301d4cb6ef03fed339
Tom Allen <tomjamesallenweb@gmail.com>
2016-01-30 12:02:14 +0000

    Adding initial test.

---
+++ test/callIdCounter.js
@@ -0,0 +1,29 @@
+var assert = require('assert');
+var AHM = require('../index.js');
+
+describe('callIdCounter', function () {
+
+  // Create a new instance of AHM.
+  var myHookManager = AHM();
+
+  it('should start with a null value', function () {
+    // Check that last call id was null.
+    assert.equal(null, myHookManager.getLastCallId());
+  });
+
+  it('should be 0 after first call', function () {
+    // Make a call.
+    myHookManager.makeCall();
+
+    // Check that last call id is now 0.
+    assert.equal(0, myHookManager.getLastCallId());
+  });
+
+  it('should be 1 after second call', function () {    
+    // Make another call.
+    myHookManager.makeCall();
+
+    // Check that last call id is now 1.
+    assert.equal(1, myHookManager.getLastCallId());
+  });
+});
