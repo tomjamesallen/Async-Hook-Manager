ff1175b456bf6f695edb744ed7f213f84dd3c9be
Tom Allen <tomjamesallenweb@gmail.com>
2016-01-31 18:32:07 +0000

    Writing tests for hook return values.

---
+++ test/hookReturnValues.js
@@ -0,0 +1,102 @@
+var expect = require('chai').expect;
+
+var Q = require('q');
+var AHM = require('../async-hook-manager.js');
+
+describe('hooks return values', function () {
+  describe('hook that returns true', function () {
+    var myHookManager = AHM();
+
+    myHookManager.registerHook(function () {
+      return true;
+    });
+
+    it('should call the call promise`s `then` method', function (done) {
+
+      var callPromise = myHookManager.makeCall();
+      callPromise
+        .then(function () {
+          done();
+        })
+        .fail(function () {
+          var err = new Error('call was rejected');
+          done(err);
+        })
+    });
+  });
+
+  describe('hook that returns false', function () {
+    var myHookManager = AHM();
+
+    myHookManager.registerHook(function () {
+      return false;
+    });
+
+    it('should call the call promise`s `fail` method', function (done) {
+
+      var callPromise = myHookManager.makeCall();
+      callPromise
+        .then(function () {
+          var err = new Error('call was not rejected');
+          done(err);
+        })
+        .fail(function () {
+          done();
+        })
+    });
+  });
+
+  describe('hook that returns a resolving promise', function () {
+    var myHookManager = AHM();
+
+    myHookManager.registerHook(function () {
+      var qPromise = Q.defer();
+
+      setTimeout(function () {
+        qPromise.resolve();
+      }, 3);
+
+      return qPromise.promise;
+    });
+
+    it('should call the call promise`s `then` method', function (done) {
+
+      var callPromise = myHookManager.makeCall();
+      callPromise
+        .then(function () {
+          done();
+        })
+        .fail(function () {
+          var err = new Error('call was rejected');
+          done(err);
+        })
+    });
+  });
+
+  describe('hook that returns a rejected promise', function () {
+    var myHookManager = AHM();
+
+    myHookManager.registerHook(function () {
+      var qPromise = Q.defer();
+
+      setTimeout(function () {
+        qPromise.reject();
+      }, 3);
+
+      return qPromise.promise;
+    });
+
+    it('should call the call promise`s `fail` method', function (done) {
+
+      var callPromise = myHookManager.makeCall();
+      callPromise
+        .then(function () {
+          var err = new Error('call was not rejected');
+          done(err);
+        })
+        .fail(function () {
+          done();
+        })
+    });
+  });
+});
--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 require('./callIdCounter');
 require('./registerUnregisterHook');
 require('./callsAllRegisteredHooks');
+require('./hookReturnValues');
 // require('./basicSingleHookCase');
 // require('./asyncSingleHookCase');
