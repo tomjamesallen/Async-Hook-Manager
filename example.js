5ff3244c7369e923262d3c85923ee36a3da78564
Tom Allen <tomjamesallenweb@gmail.com>
2016-02-07 10:36:46 +0000

    Adding tonic example.

---
+++ example.js
@@ -0,0 +1,62 @@
+// Mock functions.
+var example = {
+  fireTransitionalAnimation: function (callback) {
+    console.log('fireTransitionalAnimation');
+    setTimeout(callback, 1500);
+  },
+  updateSomeDataSynchronously: function () {
+    console.log('updateSomeDataSynchronously');
+  },
+  updateTheCurrentRoute: function () {
+    console.log('update current route');
+  }
+};
+
+// We're using Q to return promises to the hook manager.
+var Q = require('q');
+
+// Load up AHM.
+var AHM = require('./async-hook-manager.js');
+
+// Create a new instance of AHM.
+var myHookManager = AHM();
+
+// Register an async hook.
+// The return value is the ID of the hook, which can later be used to
+// unregister the hook.
+var asyncHookId = myHookManager.registerHook(function () {
+
+  // Create a new promise.
+  var hookPromise = Q.defer();
+  
+  // Do some aync stuff...
+  example.fireTransitionalAnimation(function () {
+    
+    // On animation complete.
+    hookPromise.resolve();
+  });
+
+  return hookPromise.promise;
+});
+
+// Register a sync hook.
+var syncHook = myHookManager.registerHook(function () {
+
+  // Do something synchronously here...
+  example.updateSomeDataSynchronously();
+
+  return true;
+});
+
+// Make a call via the hook manager instance.
+// This will call all of the registered hooks, and wait for all to respond
+// before firing its callback.
+myHookManager.makeCall().then(function () {
+
+  // Do something that has to wait for all of the hooks to return.
+  example.updateTheCurrentRoute();
+
+});
+
+// Unregister a hook that is no longer required.
+myHookManager.unregisterHook(syncHook);
